c350c2a8525211155236918385f32c8a4dc74f17
kainousone sipasith <kainousonesps@gmail.com>
2024-02-06 15:22:11 +0700

    first add

---
+++ javascrip.js
@@ -0,0 +1,38 @@
+
+function onYesClick() {
+    document.getElementById('noButton').style.display = ''; // Hide the No button
+    document.getElementById('displayedImage').src = "hfull.jpg";
+    document.getElementById('yesButton').classList.add('smaller');
+  }
+  
+
+  var clickCount = 0;
+
+function onChangeClick() {
+    // Get the displayed image element
+    var displayedImage = document.getElementById('displayedImage');
+    document.getElementById('yesButton').classList.add('bigger');
+
+    // Toggle between two images based on the click count
+    if (clickCount  === 0) {
+        displayedImage.src = "holding kerb f.png";
+        document.getElementById("demo").innerHTML ="hai o krd eik thuea "
+    } else{
+        displayedImage.src = "heart broken.png";
+        document.getElementById("demo").innerHTML ="elee wa T-T"
+    }
+    
+    // Increment the click count
+    clickCount++;
+}
+function onNoClick() {
+    // Remove the 'bigger' class from the 'yesButton' element
+    document.getElementById('yesButton').classList.remove('bigger');
+
+    // Hide the No button
+    document.getElementById('noButton').style.display = 'none';
+
+    // Return default values
+    return getDefaultValues();
+}
+  
